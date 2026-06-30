993608fe546ba00a66ca33eb17172e3a4f5064e7
SpencerLS <spencerliamsmith@gmail.com>
2026-06-30 00:01:42 -0600

    transparency for windows

---
+++ nvim/lua/config/transparency.lua
@@ -0,0 +1,18 @@
+
+local function make_transparent()
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+  vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+  vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
+  vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+  vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+  vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+  vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
+end
+
+make_transparent()
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = make_transparent,
+})
