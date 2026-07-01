e21c3f685536924c654f2f57487372dbf0bcc114
Spencer Smith <spencerliamsmith@gmail.com>
2026-07-01 03:41:26 -0600

    update remaps

--- nvim/lua/spencerls/remap.lua
+++ nvim/lua/spencerls/remap.lua
@@ -1,14 +1,17 @@
-vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
-
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+vim.keymap.set("n", "<leader>fe", "<cmd>Oil<CR>", { desc = "Open file explorer" })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
+
+vim.keymap.set("n", "Y", "yg$", { desc = "Yank to end of line" })
 vim.keymap.set("n", "J", "mzJ`z", { desc = "Join line and keep cursor" })
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half-page down centered" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half-page up centered" })
+
 vim.keymap.set("n", "n", "nzzzv", { desc = "Next search centered" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search centered" })
 
@@ -16,3 +19,26 @@ vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without replacing reg
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboard" })
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]], { desc = "Delete without replacing register" })
+
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Escape insert mode" })
+
+vim.keymap.set("n", "Q", "<nop>", { desc = "Avoid worst place in the universe"})
+
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz", { desc = "Next quickfix item" })
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz", { desc = "Previous quickfix item" })
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz", { desc = "Next location item" })
+vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz", { desc = "Previous location item" })
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search and replace" })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make file executable" })
+
+
+
+
+
+
+
+
+
