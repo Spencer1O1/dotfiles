f2f6cfe8990d2056ffeeedf970e6708965c98dd2
Spencer Smith <spencerliamsmith@gmail.com>
2026-07-01 03:12:25 -0600

    update settings

--- nvim/lua/spencerls/set.lua
+++ nvim/lua/spencerls/set.lua
@@ -2,17 +2,28 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
 vim.opt.termguicolors = true
 
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
