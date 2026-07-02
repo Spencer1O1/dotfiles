4a556baa8925d6ea2f84c6b3120cda5cdfac4432
Spencer Smith <spencerliamsmith@gmail.com>
2026-07-01 18:04:48 -0600

    lualine

---
+++ nvim/lua/spencerls/plugins/lualine.lua
@@ -0,0 +1,38 @@
+local function transparent(theme)
+	for _, section in pairs(theme) do
+		if section.c then
+			section.c.bg = nil
+		end
+	end
+	return theme
+end
+
+return {
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"folke/tokyonight.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		lazy = false,
+		opts = {
+			options = {
+				theme = transparent(require("lualine.themes.tokyonight")),
+				globalstatus = true,
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = { "dashboard", "alpha", "lazy" },
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { { "filename", path = 1 } },
+				lualine_x = { "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		},
+	},
+}
